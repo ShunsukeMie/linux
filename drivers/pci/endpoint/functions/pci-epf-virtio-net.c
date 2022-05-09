6d8f99f72e38c924d78f84693c037cb1198d59d7
Shunsuke Mie <mie@igel.co.jp>
2022-05-09 17:06:39 +0900

    pci: epf: virnet: introduce a pci epf virtio-net function driver
    [wip]: add initial code for epf
    
    wip: enable to show a virtio-net device
    
    There are bug in virtio transitional legacy device id.
    
    https://lore.kernel.org/kvm/20210531072743.363171-2-lingshan.zhu@intel.com/
    
    It must be hex values.
    
    wip: initialize virtio net device

---
+++ drivers/pci/endpoint/functions/pci-epf-virtio-net.c
@@ -0,0 +1,248 @@
+/*
+ * Endpoint function driver to implement pci virtio-net functionality.
+ *
+ */
+
+#include <linux/module.h>
+#include <linux/pci-epf.h>
+#include <linux/pci-epc.h>
+#include <linux/virtio_ids.h>
+#include <linux/virtio_net.h>
+
+//TODO care for native endianess
+struct virtio_common_config {
+	uint32_t dev_feat;
+	uint32_t drv_feat;
+	uint32_t q_addr;
+	uint16_t q_size;
+	uint16_t q_select;
+	uint16_t q_notify;
+	uint8_t dev_status;
+	uint8_t isr_status;
+} __packed;
+
+struct epf_virtnet {
+	struct pci_epf *epf;
+	struct {
+		struct virtio_common_config common_cfg;
+		struct virtio_net_config net_cfg;
+	} __packed *pci_config;
+	const struct pci_epc_features *epc_features;
+};
+
+static int epf_virtnet_setup_bar(struct pci_epf *epf)
+{
+	struct pci_epc *epc = epf->epc;
+	const enum pci_barno cfg_bar = BAR_0;
+	struct pci_epf_bar *virt_cfg_bar = &epf->bar[cfg_bar];
+	struct epf_virtnet *vnet = epf_get_drvdata(epf);
+	size_t cfg_bar_size = sizeof(struct virtio_common_config) +
+			      sizeof(struct virtio_net_config);
+	const struct pci_epc_features *epc_features = vnet->epc_features;
+	void *cfg_base;
+	int ret;
+
+	if (!!(epc_features->reserved_bar & (1 << cfg_bar)))
+		return -EOPNOTSUPP;
+
+	if (epc_features->bar_fixed_size[cfg_bar]) {
+		if (cfg_bar_size > epc_features->bar_fixed_size[cfg_bar])
+			return -ENOMEM;
+
+		cfg_bar_size = epc_features->bar_fixed_size[cfg_bar];
+	}
+
+	cfg_base = pci_epf_alloc_space(epf, cfg_bar_size, cfg_bar, epc_features->align, PRIMARY_INTERFACE);
+	if (!cfg_base) {
+		pr_err("Failed to allocate PCI BAR memory\n");
+		return -ENOMEM;
+	}
+	vnet->pci_config = cfg_base;
+
+	ret = pci_epc_set_bar(epc, epf->func_no, epf->vfunc_no, virt_cfg_bar);
+	if (ret) {
+		pr_err("Failed to set PCI BAR\n");
+		return ret;
+	}
+
+	return 0;
+}
+
+static int epf_virtnet_load_epc_features(struct pci_epf *epf)
+{
+	const struct pci_epc_features *epc_features;
+	struct epf_virtnet *epf_virtnet = epf_get_drvdata(epf);
+	struct pci_epc *epc = epf->epc;
+
+	epc_features = pci_epc_get_features(epc, epf->func_no, epf->vfunc_no);
+	if (!epc_features) {
+		pr_err("epc_features not implemented\n");
+		return -EOPNOTSUPP;
+	}
+
+	epf_virtnet->epc_features = epc_features;
+
+	return 0;
+}
+
+static int epf_virtnet_setup_interrupt(struct pci_epf *epf)
+{
+	int ret;
+// 	const enum pci_barno cfg_bar = BAR_0;
+	struct epf_virtnet *epf_virtnet = epf_get_drvdata(epf);
+	const struct pci_epc_features *epc_features = epf_virtnet->epc_features;
+	struct pci_epc *epc = epf->epc;
+
+	if (epc_features->msi_capable) {
+		ret = pci_epc_set_msi(epc, epf->func_no, epf->vfunc_no, 1);
+		if (ret) {
+			pr_err("MSI configuration failed\n");
+			return ret;
+		}
+	}
+
+// TODO msix support.
+// 	if (epc_features->msix_capable) {
+// 		ret = pci_epc_set_msix(epc, epf->func_no, epf->vfunc_no,
+// 				       epf->msix_interrupts,
+// 				       cfg_bar,
+// 				       epf_test->msix_table_offset);
+// 		if (ret) {
+// 			pr_err("MSI-X configuration failed\n");
+// 			return ret;
+// 		}
+// 	}
+
+	return 0;
+}
+
+#define EPF_VIRTNET_Q_SIZE 32
+
+static void epf_virtnet_init_config(struct pci_epf *epf)
+{
+	struct epf_virtnet *vnet = epf_get_drvdata(epf);
+	struct virtio_common_config *common_cfg = &vnet->pci_config->common_cfg;
+	struct virtio_net_config *net_cfg = &vnet->pci_config->net_cfg;
+
+	//TODO consider the device feature
+	//TODO care about endianness (must be guest(root complex) endianness)
+	common_cfg->dev_feat = BIT(VIRTIO_NET_F_MAC) | BIT(VIRTIO_NET_F_GUEST_CSUM);
+	common_cfg->q_select = 0;
+	common_cfg->q_size = EPF_VIRTNET_Q_SIZE;
+	common_cfg->q_notify = 2;
+	common_cfg->isr_status = 1;
+
+	net_cfg->max_virtqueue_pairs = 1;
+	// TODO fix tempolary mac address
+	u8 mac[ETH_ALEN] = {0, 0, 0, 0, 0, 0};
+	memcpy(net_cfg->mac, mac, ETH_ALEN);
+}
+
+static int epf_virtnet_bind(struct pci_epf *epf)
+{
+	int ret;
+	struct pci_epc *epc = epf->epc;
+
+	ret = epf_virtnet_load_epc_features(epf);
+	if (ret) {
+		pr_err("Load epc feature failed\n");
+		return ret;
+	}
+
+	ret = pci_epc_write_header(epc, epf->func_no, epf->vfunc_no, epf->header);
+	if (ret) {
+		pr_err("Configuration header write failed\n");
+		return ret;
+	}
+
+	ret = epf_virtnet_setup_bar(epf);
+	if (ret) {
+		pr_err("PCI bar set failed\n");
+		return ret;
+	}
+
+	ret = epf_virtnet_setup_interrupt(epf);
+	if (ret) {
+		pr_err("Interrupt setup failed\n");
+		return ret;
+	}
+
+	epf_virtnet_init_config(epf);
+
+	return 0;
+}
+
+static void epf_virtnet_unbind(struct pci_epf *epf)
+{
+}
+
+static struct pci_epf_ops epf_virtnet_ops = {
+	.bind = epf_virtnet_bind,
+	.unbind = epf_virtnet_unbind,
+};
+
+static struct pci_epf_header epf_virtnet_header = {
+	.vendorid = PCI_VENDOR_ID_REDHAT_QUMRANET,
+	.deviceid = VIRTIO_TRANS_ID_NET,
+	.subsys_id = VIRTIO_ID_NET,
+	.revid = 0,
+ 	.baseclass_code = PCI_BASE_CLASS_NETWORK, //TODO consider
+// 	.subclass_code = , //TODO add subclasse? like 00 ethernet
+	.interrupt_pin = PCI_INTERRUPT_INTA,
+};
+
+static int epf_virtnet_probe(struct pci_epf *epf)
+{
+	struct epf_virtnet *vnet;
+	struct device *dev;
+
+	dev = &epf->dev;
+
+	vnet = devm_kzalloc(dev, sizeof(*vnet), GFP_KERNEL);
+	if (!vnet)
+		return -ENOMEM;
+
+	epf->header = &epf_virtnet_header;
+	vnet->epf = epf;
+	epf_set_drvdata(epf, vnet);
+
+	return 0;
+}
+
+static const struct pci_epf_device_id epf_virtnet_ids[] = {
+	{
+		.name = "pci_epf_virtio_net"
+	},
+	{},
+};
+
+static struct pci_epf_driver virtnet_driver = {
+	.driver.name = "pci_epf_virtio_net",
+	.ops = &epf_virtnet_ops,
+	.id_table = epf_virtnet_ids,
+	.probe = epf_virtnet_probe,
+	.owner = THIS_MODULE
+};
+
+static int __init epf_virtnet_init(void)
+{
+	int ret;
+
+	ret = pci_epf_register_driver(&virtnet_driver);
+	if (ret) {
+		pr_err("Failed to register pci epf virtio-net driver: %d\n",
+		       ret);
+		return ret;
+	}
+
+	return 0;
+}
+module_init(epf_virtnet_init);
+
+static void epf_virtnet_exit(void)
+{
+	pci_epf_unregister_driver(&virtnet_driver);
+}
+module_exit(epf_virtnet_exit);
+
+MODULE_LICENSE("GPL");
