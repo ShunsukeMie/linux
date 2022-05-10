15f47110cc272fa5f0f25302dc054cf490bed6a8
Shunsuke Mie <mie@igel.co.jp>
2022-05-10 18:35:34 +0900

    wip: enable to show a virtio-net device
    There are bug in virtio transitional legacy device id.
    
    https://lore.kernel.org/kvm/20210531072743.363171-2-lingshan.zhu@intel.com/
    
    It must be hex values.

--- drivers/pci/endpoint/functions/pci-epf-virtio-net.c
+++ drivers/pci/endpoint/functions/pci-epf-virtio-net.c
@@ -5,38 +5,86 @@
 
 #include <linux/module.h>
 #include <linux/pci-epf.h>
+#include <linux/pci-epc.h>
+#include <linux/virtio_ids.h>
 
-static int epf_virtio_net_bind(struct pci_epf *epf)
-{
-	return 0;
-}
-
-static void epf_virtio_net_unbind(struct pci_epf *epf)
-{
-}
-
-static struct pci_epf_ops epf_virtio_net_ops = {
-	.bind = epf_virtio_net_bind,
-	.unbind = epf_virtio_net_unbind,
+struct epf_virtnet {
+	struct pci_epf *epf;
 };
 
-static int epf_virtio_net_probe(struct pci_epf *epf)
+static int epf_virtnet_bind(struct pci_epf *epf)
 {
+	int ret;
+	const struct pci_epc_features *epc_features;
+	enum pci_barno bar;
+	struct pci_epc *epc = epf->epc;
+
+	ret = pci_epc_write_header(epc, epf->func_no, epf->vfunc_no, epf->header);
+	if (ret) {
+		pr_err("Configuration header write failed\n");
+		return ret;
+	}
+
 	return 0;
 }
 
-static struct pci_epf_driver virtio_net_driver = {
+static void epf_virtnet_unbind(struct pci_epf *epf)
+{
+}
+
+static struct pci_epf_ops epf_virtnet_ops = {
+	.bind = epf_virtnet_bind,
+	.unbind = epf_virtnet_unbind,
+};
+
+static struct pci_epf_header epf_virtnet_header = {
+	.vendorid = PCI_VENDOR_ID_REDHAT_QUMRANET,
+	.deviceid = VIRTIO_TRANS_ID_NET,
+	.subsys_id = VIRTIO_ID_NET,
+	.revid = 0,
+ 	.baseclass_code = PCI_BASE_CLASS_NETWORK, //TODO consider
+// 	.subclass_code = , //TODO add subclasse? like 00 ethernet
+	.interrupt_pin = PCI_INTERRUPT_INTA,
+};
+
+static int epf_virtnet_probe(struct pci_epf *epf)
+{
+	struct epf_virtnet *vnet;
+	struct device *dev;
+
+	dev = &epf->dev;
+
+	vnet = devm_kzalloc(dev, sizeof(*vnet), GFP_KERNEL);
+	if (!vnet)
+		return -ENOMEM;
+
+	epf->header = &epf_virtnet_header;
+	vnet->epf = epf;
+	epf_set_drvdata(epf, vnet);
+
+	return 0;
+}
+
+static const struct pci_epf_device_id epf_virtnet_ids[] = {
+	{
+		.name = "pci_epf_virtio_net"
+	},
+	{},
+};
+
+static struct pci_epf_driver virtnet_driver = {
 	.driver.name = "pci_epf_virtio_net",
-	.ops = &epf_virtio_net_ops,
-	.probe = epf_virtio_net_probe,
+	.ops = &epf_virtnet_ops,
+	.id_table = epf_virtnet_ids,
+	.probe = epf_virtnet_probe,
 	.owner = THIS_MODULE
 };
 
-static int __init epf_virtio_net_init(void)
+static int __init epf_virtnet_init(void)
 {
 	int ret;
 
-	ret = pci_epf_register_driver(&virtio_net_driver);
+	ret = pci_epf_register_driver(&virtnet_driver);
 	if (ret) {
 		pr_err("Failed to register pci epf virtio-net driver: %d\n",
 		       ret);
@@ -45,11 +93,12 @@ static int __init epf_virtio_net_init(void)
 
 	return 0;
 }
-module_init(epf_virtio_net_init);
+module_init(epf_virtnet_init);
 
-static void epf_virtio_net_exit(void)
+static void epf_virtnet_exit(void)
 {
+	pci_epf_unregister_driver(&virtnet_driver);
 }
-module_exit(epf_virtio_net_exit);
+module_exit(epf_virtnet_exit);
 
 MODULE_LICENSE("GPL");
