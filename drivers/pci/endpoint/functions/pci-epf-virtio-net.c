1220487d37b1545ed0f91b4beb56cb73e4f6c4b5
Shunsuke Mie <mie@igel.co.jp>
2022-06-22 11:23:26 +0900

    wip:

--- drivers/pci/endpoint/functions/pci-epf-virtio-net.c
+++ drivers/pci/endpoint/functions/pci-epf-virtio-net.c
@@ -41,6 +41,20 @@ struct epf_virtnet {
 	} *vqs;
 };
 
+struct virtnet_info {
+	struct net_device *dev;
+	struct epf_virtnet *epf_vnet;
+
+	/* Max # of queue pairs supported by the device */
+	u16 max_queue_pairs;
+
+	/* # of queue pairs currently used by the driver */
+	u16 curr_queue_pairs;
+
+	/* Packet virtio header size */
+	u8 hdr_len;
+};
+
 static int epf_virtnet_setup_bar(struct pci_epf *epf)
 {
 	struct pci_epc *epc = epf->epc;
@@ -353,6 +367,264 @@ static struct pci_epf_header epf_virtnet_header = {
 // 	.subclass_code = , //TODO add subclasse? like 00 ethernet
 	.interrupt_pin = PCI_INTERRUPT_INTA,
 };
+
+static int epf_virtnet_init_netdev(struct epf_virtnet *epf_vnet)
+{
+	int i, err = -ENOMEM;
+	struct net_device *dev;
+	struct virtnet_info *vi;
+	u16 max_queue_pairs;
+	int mtu;
+
+	/* Find if host supports multiqueue/rss virtio_net device */
+	//TODO read from config space?
+	max_queue_pairs = 1;
+// 	if (virtio_has_feature(vdev, VIRTIO_NET_F_MQ) || virtio_has_feature(vdev, VIRTIO_NET_F_RSS))
+// 		max_queue_pairs =
+// 		     virtio_cread16(vdev, offsetof(struct virtio_net_config, max_virtqueue_pairs));
+
+	/* We need at least 2 queue's */
+// 	if (max_queue_pairs < VIRTIO_NET_CTRL_MQ_VQ_PAIRS_MIN ||
+// 	    max_queue_pairs > VIRTIO_NET_CTRL_MQ_VQ_PAIRS_MAX ||
+// 	    !virtio_has_feature(vdev, VIRTIO_NET_F_CTRL_VQ))
+// 		max_queue_pairs = 1;
+
+	/* Allocate ourselves a network device with room for our info */
+	dev = alloc_etherdev_mq(sizeof(struct virtnet_info), max_queue_pairs);
+	if (!dev)
+		return -ENOMEM;
+
+	/* Set up network device as normal. */
+	dev->priv_flags |= IFF_UNICAST_FLT | IFF_LIVE_ADDR_CHANGE |
+			   IFF_TX_SKB_NO_LINEAR;
+	dev->netdev_ops = &virtnet_netdev;
+	dev->features = NETIF_F_HIGHDMA;
+
+	dev->ethtool_ops = &virtnet_ethtool_ops;
+// 	SET_NETDEV_DEV(dev, &vdev->dev);
+// 	XXX:
+// 	SET_NETDEV_DEV(dev, &epf_vnet->epf->epc->dev);
+	SET_NETDEV_DEV(dev, &epf_vnet->epf->dev);
+
+	/* Do we support "hardware" checksums? */
+// 	if (virtio_has_feature(vdev, VIRTIO_NET_F_CSUM)) {
+// 		/* This opens up the world of extra features. */
+// 		dev->hw_features |= NETIF_F_HW_CSUM | NETIF_F_SG;
+// 		if (csum)
+// 			dev->features |= NETIF_F_HW_CSUM | NETIF_F_SG;
+//
+// 		if (virtio_has_feature(vdev, VIRTIO_NET_F_GSO)) {
+// 			dev->hw_features |= NETIF_F_TSO
+// 				| NETIF_F_TSO_ECN | NETIF_F_TSO6;
+// 		}
+// 		/* Individual feature bits: what can host handle? */
+// 		if (virtio_has_feature(vdev, VIRTIO_NET_F_HOST_TSO4))
+// 			dev->hw_features |= NETIF_F_TSO;
+// 		if (virtio_has_feature(vdev, VIRTIO_NET_F_HOST_TSO6))
+// 			dev->hw_features |= NETIF_F_TSO6;
+// 		if (virtio_has_feature(vdev, VIRTIO_NET_F_HOST_ECN))
+// 			dev->hw_features |= NETIF_F_TSO_ECN;
+//
+// 		dev->features |= NETIF_F_GSO_ROBUST;
+//
+// 		if (gso)
+// 			dev->features |= dev->hw_features & NETIF_F_ALL_TSO;
+// 		/* (!csum && gso) case will be fixed by register_netdev() */
+// 	}
+// 	if (virtio_has_feature(vdev, VIRTIO_NET_F_GUEST_CSUM))
+// 		dev->features |= NETIF_F_RXCSUM;
+// 	if (virtio_has_feature(vdev, VIRTIO_NET_F_GUEST_TSO4) ||
+// 	    virtio_has_feature(vdev, VIRTIO_NET_F_GUEST_TSO6))
+// 		dev->features |= NETIF_F_GRO_HW;
+// 	if (virtio_has_feature(vdev, VIRTIO_NET_F_CTRL_GUEST_OFFLOADS))
+// 		dev->hw_features |= NETIF_F_GRO_HW;
+
+	dev->vlan_features = dev->features;
+
+	/* MTU range: 68 - 65535 */
+	dev->min_mtu = MIN_MTU;
+	dev->max_mtu = MAX_MTU;
+
+	/* Configuration may specify what MAC to use.  Otherwise random. */
+// 	if (virtio_has_feature(vdev, VIRTIO_NET_F_MAC)) {
+// 		u8 addr[ETH_ALEN];
+//
+// 		virtio_cread_bytes(vdev,
+// 				   offsetof(struct virtio_net_config, mac),
+// 				   addr, ETH_ALEN);
+// 		eth_hw_addr_set(dev, addr);
+// 	} else {
+// 		eth_hw_addr_random(dev);
+// 	}
+
+	/* Set up our device-specific information */
+	vi = netdev_priv(dev);
+	vi->dev = dev;
+// 	vi->vdev = vdev;
+	vi->epf_vnet = epf_vnet;
+// 	vdev->priv = vi;
+// 	XXX: is it reuqired?
+// 	epf_vnet->net = dev;
+
+	/* If we can receive ANY GSO packets, we must allocate large ones. */
+// 	if (virtio_has_feature(vdev, VIRTIO_NET_F_GUEST_TSO4) ||
+// 	    virtio_has_feature(vdev, VIRTIO_NET_F_GUEST_TSO6) ||
+// 	    virtio_has_feature(vdev, VIRTIO_NET_F_GUEST_ECN) ||
+// 	    virtio_has_feature(vdev, VIRTIO_NET_F_GUEST_UFO))
+// 		vi->big_packets = true;
+
+// 	if (virtio_has_feature(vdev, VIRTIO_NET_F_MRG_RXBUF))
+// 		vi->mergeable_rx_bufs = true;
+//
+// 	if (virtio_has_feature(vdev, VIRTIO_NET_F_HASH_REPORT))
+// 		vi->has_rss_hash_report = true;
+//
+// 	if (virtio_has_feature(vdev, VIRTIO_NET_F_RSS))
+// 		vi->has_rss = true;
+
+// 	if (vi->has_rss || vi->has_rss_hash_report) {
+// 		vi->rss_indir_table_size =
+// 			virtio_cread16(vdev, offsetof(struct virtio_net_config,
+// 				rss_max_indirection_table_length));
+// 		vi->rss_key_size =
+// 			virtio_cread8(vdev, offsetof(struct virtio_net_config, rss_max_key_size));
+//
+// 		vi->rss_hash_types_supported =
+// 		    virtio_cread32(vdev, offsetof(struct virtio_net_config, supported_hash_types));
+// 		vi->rss_hash_types_supported &=
+// 				~(VIRTIO_NET_RSS_HASH_TYPE_IP_EX |
+// 				  VIRTIO_NET_RSS_HASH_TYPE_TCP_EX |
+// 				  VIRTIO_NET_RSS_HASH_TYPE_UDP_EX);
+//
+// 		dev->hw_features |= NETIF_F_RXHASH;
+// 	}
+
+// 	if (vi->has_rss_hash_report)
+// 		vi->hdr_len = sizeof(struct virtio_net_hdr_v1_hash);
+// 	else if (virtio_has_feature(vdev, VIRTIO_NET_F_MRG_RXBUF) ||
+// 		 virtio_has_feature(vdev, VIRTIO_F_VERSION_1))
+// 		vi->hdr_len = sizeof(struct virtio_net_hdr_mrg_rxbuf);
+// 	else
+		vi->hdr_len = sizeof(struct virtio_net_hdr);
+
+// 	if (virtio_has_feature(vdev, VIRTIO_F_ANY_LAYOUT) ||
+// 	    virtio_has_feature(vdev, VIRTIO_F_VERSION_1))
+// 		vi->any_header_sg = true;
+//
+// 	if (virtio_has_feature(vdev, VIRTIO_NET_F_CTRL_VQ))
+// 		vi->has_cvq = true;
+//
+// 	if (virtio_has_feature(vdev, VIRTIO_NET_F_MTU)) {
+// 		mtu = virtio_cread16(vdev,
+// 				     offsetof(struct virtio_net_config,
+// 					      mtu));
+// 		if (mtu < dev->min_mtu) {
+// 			/* Should never trigger: MTU was previously validated
+// 			 * in virtnet_validate.
+// 			 */
+// 			dev_err(&vdev->dev,
+// 				"device MTU appears to have changed it is now %d < %d",
+// 				mtu, dev->min_mtu);
+// 			err = -EINVAL;
+// 			goto free;
+// 		}
+//
+// 		dev->mtu = mtu;
+// 		dev->max_mtu = mtu;
+//
+// 		/* TODO: size buffers correctly in this case. */
+// 		if (dev->mtu > ETH_DATA_LEN)
+// 			vi->big_packets = true;
+// 	}
+
+// 	if (vi->any_header_sg)
+// 		dev->needed_headroom = vi->hdr_len;
+
+	/* Enable multiqueue by default */
+	if (num_online_cpus() >= max_queue_pairs)
+		vi->curr_queue_pairs = max_queue_pairs;
+	else
+		vi->curr_queue_pairs = num_online_cpus();
+	vi->max_queue_pairs = max_queue_pairs;
+
+	/* Allocate/initialize the rx/tx queues, and invoke find_vqs */
+	err = init_vqs(vi);
+	if (err)
+		goto free;
+
+// #ifdef CONFIG_SYSFS
+// 	if (vi->mergeable_rx_bufs)
+// 		dev->sysfs_rx_queue_group = &virtio_net_mrg_rx_group;
+// #endif
+	netif_set_real_num_tx_queues(dev, vi->curr_queue_pairs);
+	netif_set_real_num_rx_queues(dev, vi->curr_queue_pairs);
+
+	//TODO: need to setup  vi->speed and vi->duplex;
+	//virtnet_init_settings(dev);
+
+// 	if (virtio_has_feature(vdev, VIRTIO_NET_F_STANDBY)) {
+// 		vi->failover = net_failover_create(vi->dev);
+// 		if (IS_ERR(vi->failover)) {
+// 			err = PTR_ERR(vi->failover);
+// 			goto free_vqs;
+// 		}
+// 	}
+
+// 	if (vi->has_rss || vi->has_rss_hash_report)
+// 		virtnet_init_default_rss(vi);
+
+	err = register_netdev(dev);
+	if (err) {
+		pr_debug("virtio_net: registering device failed\n");
+		goto free_failover;
+	}
+
+// 	virtio_device_ready(vdev);
+
+	err = virtnet_cpu_notif_add(vi);
+	if (err) {
+		pr_debug("virtio_net: registering cpu notifier failed\n");
+		goto free_unregister_netdev;
+	}
+
+	virtnet_set_queues(vi, vi->curr_queue_pairs);
+
+	/* Assume link up if device can't report link status,
+	   otherwise get link status from config. */
+	netif_carrier_off(dev);
+// 	if (virtio_has_feature(vi->vdev, VIRTIO_NET_F_STATUS)) {
+//		INIT_WORK(&vi->config_work, virtnet_config_changed_work);
+// 		schedule_work(&vi->config_work);
+// 	} else {
+		vi->status = VIRTIO_NET_S_LINK_UP;
+		virtnet_update_settings(vi);
+		netif_carrier_on(dev);
+// 	}
+
+// 	for (i = 0; i < ARRAY_SIZE(guest_offloads); i++)
+// 		if (virtio_has_feature(vi->vdev, guest_offloads[i]))
+// 			set_bit(guest_offloads[i], &vi->guest_offloads);
+// 	vi->guest_offloads_capable = vi->guest_offloads;
+
+	pr_info("virtnet: registered device %s with %d RX and TX vq's\n",
+		 dev->name, max_queue_pairs);
+
+	return 0;
+
+free_unregister_netdev:
+	virtio_reset_device(vdev);
+
+	unregister_netdev(dev);
+free_failover:
+	net_failover_destroy(vi->failover);
+free_vqs:
+	cancel_delayed_work_sync(&vi->refill);
+	free_receive_page_frags(vi);
+	virtnet_del_vqs(vi);
+free:
+	free_netdev(dev);
+	return err;
+}
 
 static int epf_virtnet_probe(struct pci_epf *epf)
 {
