de1532e2b3cc105e02a69c28fe1e71c3015ba851
Shunsuke Mie <mie@igel.co.jp>
2023-01-19 15:32:14 +0900

    wip: support dma

--- drivers/pci/endpoint/functions/pci-epf-vnet.c
+++ drivers/pci/endpoint/functions/pci-epf-vnet.c
@@ -5,7 +5,9 @@
  */
 #include <linux/module.h>
 #include <linux/pci-epf.h>
+#include <linux/pci-epc.h>
 #include <linux/vringh.h>
+#include <linux/dmaengine.h>
 
 #include "pci-epf-vnet.h"
 
@@ -17,28 +19,131 @@ int epf_vnet_get_vq_size(void)
 	return virtio_queue_size;
 }
 
-int epf_vnet_transfer(struct epf_vnet *vnet, enum epf_vnet_tx_dir dir)
+struct epf_dma_filter_param {
+	struct device *dev;
+	u32 dma_mask;
+};
+
+static bool epf_virtnet_dma_filter(struct dma_chan *chan, void *param)
 {
+	struct epf_dma_filter_param *fparam = param;
+	struct dma_slave_caps caps;
+
+	memset(&caps, 0, sizeof caps);
+	dma_get_slave_caps(chan, &caps);
+
+	return chan->device->dev == fparam->dev &&
+	       (fparam->dma_mask & caps.directions);
+}
+
+static int epf_vnet_init_edma(struct epf_vnet *vnet, struct device *dma_dev)
+{
+	dma_cap_mask_t mask;
+	struct epf_dma_filter_param param;
+
+	dma_cap_zero(mask);
+	dma_cap_set(DMA_SLAVE, mask);
+
+	param.dev = dma_dev;
+	param.dma_mask = BIT(DMA_MEM_TO_DEV);
+	vnet->lr_dma_chan =
+		dma_request_channel(mask, epf_virtnet_dma_filter, &param);
+	if (!vnet->lr_dma_chan) {
+		pr_info("failed to request dma channel\n");
+		return -EOPNOTSUPP;
+	}
+
+	param.dma_mask = BIT(DMA_DEV_TO_MEM);
+	vnet->rl_dma_chan =
+		dma_request_channel(mask, epf_virtnet_dma_filter, &param);
+	if (!vnet->rl_dma_chan) {
+		pr_info("failed to request dma channel\n");
+		return -EOPNOTSUPP;
+	}
+
+	vnet->use_dma = true;
+
+	return 0;
+	//TODO error handling
+}
+
+static int epf_vnet_dma_single(struct epf_vnet *vnet, phys_addr_t pci,
+			       dma_addr_t dma, size_t len,
+			       void (*callback)(void *), void *param,
+			       enum dma_transfer_direction dir)
+{
+	struct dma_async_tx_descriptor *desc;
+	int err;
+	struct dma_chan *chan;
+	struct dma_slave_config sconf;
+	dma_cookie_t cookie;
+	unsigned long flags = 0;
+
+	if (DMA_MEM_TO_DEV == dir) {
+		sconf.dst_addr = pci;
+		chan = vnet->lr_dma_chan;
+	} else {
+		sconf.src_addr = pci;
+		chan = vnet->rl_dma_chan;
+	}
+
+	err = dmaengine_slave_config(chan, &sconf);
+	if (unlikely(err)) {
+		pr_err("failed to setup");
+		return err;
+	}
+
+	if (callback)
+		flags = DMA_PREP_INTERRUPT | DMA_PREP_FENCE;
+
+	desc = dmaengine_prep_slave_single(chan, dma, len, dir, flags);
+	if (unlikely(!desc))
+		return EIO;
+
+	desc->callback = callback;
+	desc->callback_param = param;
+
+	cookie = dmaengine_submit(desc);
+	err = dma_submit_error(cookie);
+	if (unlikely(err)) {
+		pr_err("failed to submit dma\n");
+		return err;
+	}
+
+	dma_async_issue_pending(chan);
+
+	return 0;
+}
+
+struct epf_vnet_dma_callback_param {
 	struct vringh *tx_vrh, *rx_vrh;
-	struct vringh_kiov *tx_iov, *rx_iov;
+	size_t total_len;
+	u16 tx_head, rx_head;
+};
+
+static void epf_vnet_dma_callback(void *p)
+{
+	struct epf_vnet_dma_callback_param *param = p;
+	pr_info("callback is called!\n");
+
+	vringh_complete(param->tx_vrh, param->tx_head, param->total_len);
+	vringh_complete(param->rx_vrh, param->rx_head, param->total_len);
+
+	//TODO interrupt to each drivers
+
+	kfree(param);
+}
+
+int epf_vnet_transfer(struct epf_vnet *vnet, struct vringh *tx_vrh,
+		      struct vringh *rx_vrh, struct vringh_kiov *tx_iov,
+		      struct vringh_kiov *rx_iov,
+		      enum dma_transfer_direction dir)
+{
 	int err;
 	u16 tx_head, rx_head;
 	size_t total_tx_len;
-
-	switch (dir) {
-	case EPF_VNET_TX_DIR_EP_TO_RC:
-		tx_vrh = &vnet->rc.txvrh->vrh;
-		// 			rx_vrh = &vnet->ep.rxvrh->vrh;
-		tx_iov = &vnet->rc.tx_iov;
-		// 			rx_iov = &vnet->ep.rx_iov;
-		break;
-	case EPF_VNET_TX_DIR_RC_TO_EP:
-		// 			tx_vrh = &vnet->ep.txvrh->vrh;
-		rx_vrh = &vnet->rc.rxvrh->vrh;
-		// 			tx_iov = &vnet->ep.tx_iov;
-		rx_iov = &vnet->rc.rx_iov;
-		break;
-	}
+	struct epf_vnet_dma_callback_param *cb_param;
+	struct vringh_kiov *liov, *riov;
 
 	err = vringh_getdesc(tx_vrh, tx_iov, NULL, &tx_head);
 	if (err < 0) {
@@ -60,12 +165,45 @@ int epf_vnet_transfer(struct epf_vnet *vnet, enum epf_vnet_tx_dir dir)
 		return 0;
 	}
 
-	// TODO the rx_iov range should be checked.
-	for (; tx_iov->i < tx_iov->used; tx_iov->i++, rx_iov->i++) {
+	cb_param = kmalloc(sizeof *cb_param, GFP_KERNEL);
+	if (!cb_param)
+		return -ENOMEM;
+
+	cb_param->tx_vrh = tx_vrh;
+	cb_param->rx_vrh = rx_vrh;
+	cb_param->tx_head = tx_head;
+	cb_param->rx_head = rx_head;
+	cb_param->total_len = total_tx_len;
+
+	switch(dir) {
+		case DMA_MEM_TO_DEV:
+			liov = tx_iov;
+			riov = rx_iov;
+			break;
+		case DMA_DEV_TO_MEM:
+			liov = rx_iov;
+			riov = tx_iov;
+			break;
+		default:
+			return -EINVAL;
 	}
 
-	vringh_complete(tx_vrh, tx_head, total_tx_len);
-	vringh_complete(rx_vrh, rx_head, total_tx_len);
+	// TODO the rx_iov range should be checked.
+	for (; tx_iov->i < tx_iov->used; tx_iov->i++, rx_iov->i++) {
+		size_t len;
+		u64 lbase, rbase;
+		void (*callback)(void *) = NULL;
+
+		lbase = (u64)liov->iov[liov->i].iov_base;
+		rbase = (u64)riov->iov[riov->i].iov_base;
+		len = tx_iov->iov[tx_iov->i].iov_len;
+
+		if (tx_iov->i + 1 == tx_iov->used)
+			callback = epf_vnet_dma_callback;
+
+		epf_vnet_dma_single(vnet, rbase, lbase, len, callback, cb_param,
+				    dir);
+	}
 
 	return 1;
 }
@@ -119,6 +257,7 @@ static void epf_vnet_virtio_init(struct epf_vnet *vnet)
 static int epf_vnet_probe(struct pci_epf *epf)
 {
 	struct epf_vnet *vnet;
+	int err;
 
 	vnet = devm_kzalloc(&epf->dev, sizeof *vnet, GFP_KERNEL);
 	if (!vnet)
@@ -128,6 +267,10 @@ static int epf_vnet_probe(struct pci_epf *epf)
 	vnet->epf = epf;
 
 	epf_vnet_virtio_init(vnet);
+
+	err = epf_vnet_init_edma(vnet, epf->epc->dev.parent);
+	if (err)
+		pr_info("Cannot found PCIe Embedded DMA controller. Fallback to CPU transfer.\n");
 
 	return 0;
 }
--- drivers/pci/endpoint/functions/pci-epf-vnet.h
+++ drivers/pci/endpoint/functions/pci-epf-vnet.h
@@ -5,14 +5,17 @@
 #include <linux/pci-epf.h>
 #include <linux/pci-epf-virtio.h>
 #include <linux/virtio_net.h>
-
-enum epf_vnet_tx_dir { EPF_VNET_TX_DIR_EP_TO_RC, EPF_VNET_TX_DIR_RC_TO_EP };
+#include <linux/dmaengine.h>
 
 struct epf_vnet {
 	//TODO Should this variable be placed here?
 	struct pci_epf *epf;
 	struct pci_epf_virtio virtio;
 	struct virtio_net_config vnet_cfg;
+
+	// dma channels, local to remote(lr) and remote to local(rl)
+	struct dma_chan *lr_dma_chan, *rl_dma_chan;
+	bool use_dma;
 
 	struct {
 		void __iomem *cfg_base;
@@ -23,9 +26,17 @@ struct epf_vnet {
 		struct pci_epf_vringh *txvrh, *rxvrh;
 		struct vringh_kiov tx_iov, rx_iov;
 	} rc;
+
+	struct {
+		struct pci_epf_vringh *txvrh, *rxvrh;
+		struct vringh_kiov tx_iov, rx_iov;
+	} ep;
 };
 
 int epf_vnet_rc_setup(struct epf_vnet *vnet);
 int epf_vnet_get_vq_size(void);
+int epf_vnet_transfer(struct epf_vnet *vnet, struct vringh *tx_vrh, struct vringh *rx_vrh,
+		struct vringh_kiov *tx_iov, struct vringh_kiov *rx_iov,
+		enum dma_transfer_direction dir);
 
 #endif // _PCI_EPF_VNET_H
